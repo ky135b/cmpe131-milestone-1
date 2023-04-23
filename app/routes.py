0fd223e6cf4fc9447201efdbd15b62f41add2504
natalie <natalie.ky.leung@gmail.com>
2023-04-23 01:32:06 -0700

    updated sign_In and todo html files for basic structure of the website pages

--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,28 @@ from flask_login import current_user
 from flask_login import login_user
 from flask_login import logout_user
 from flask_login import login_required
-
 @myapp_obj.route("/")
-@myapp_obj.route("/index.html")
+@myapp_obj.route("/todo.html")
+def todo():
+    return render_template('todo.html')
+@myapp_obj.route("/sign_In.html", methods=['GET', 'POST'])
+def login():
+    # create form
+    form = LoginForm()
+    # if form inputs are valid
+    if form.validate_on_submit():
+        # search database for username
+        # user = User.query.filter_by(...)
+        # check the password
+        # if password matches
+        # login_user(user)
+        flash(f'Here are the input {form.username.data} and {form.password.data}')
+        return redirect('/index')
+    return render_template('sign_In.html', form=form)
+
+@myapp_obj.route("/members/<string:name>/")
+def getMember(name):
+    return escape(name)
+@myapp_obj.route("/index")
 def index():
     return render_template('index.html')
-@myapp_obj.route("/sign_In.html")
-def index():
-    return render_template('sign_In.html')
