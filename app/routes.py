cd3511047535d0596e1396fda354c08f8ab18563
natalie <natalie.ky.leung@gmail.com>
2023-04-24 07:21:23 -0700

    added todo button and redirect to home page from sign in

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,21 @@ from flask import render_template
 from flask import redirect
 from flask import flash
 from .forms import LoginForm
+from .forms import TodoForm
 from app import myapp_obj
 from flask_login import current_user
 from flask_login import login_user
 from flask_login import logout_user
 from flask_login import login_required
 @myapp_obj.route("/")
-@myapp_obj.route("/todo.html")
-def todo():
-    return render_template('todo.html')
-@myapp_obj.route("/sign_In.html", methods=['GET', 'POST'])
+@myapp_obj.route("/index.html")
+def index():
+    form = TodoForm()
+    if form.validate_on_submit():
+        flash('validate')
+        return redirect("/todo")
+    return render_template('index.html', form = form)
+@myapp_obj.route("/sign_In", methods=['GET', 'POST'])
 def login():
     # create form
     form = LoginForm()
@@ -23,12 +28,12 @@ def login():
         # if password matches
         # login_user(user)
         flash(f'Here are the input {form.username.data} and {form.password.data}')
-        return redirect('/index')
+        return redirect('/')
     return render_template('sign_In.html', form=form)
 
 @myapp_obj.route("/members/<string:name>/")
 def getMember(name):
     return escape(name)
-@myapp_obj.route("/index")
-def index():
-    return render_template('index.html')
+@myapp_obj.route("/todo")
+def todo():
+    return render_template('todo.html')
