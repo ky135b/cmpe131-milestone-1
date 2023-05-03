ec015bd9382eea19714963fb4dc9f91725f75b89
rudysuarez1 <rudysuarez.ita@gmail.com>
2023-05-02 22:32:16 -0700

    added models.py back with message model

---
+++ app/models.py
@@ -0,0 +1,66 @@
+### IF THIS FILE IS MODIFIED, RUN tables.py TO RECREATE TABLES
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    sent_messages = db.relationship('Message', foreign_keys='Message.sender_id', backref='author', lazy='dynamic')
+    received_messages = db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
+
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f'<user {self.id}: {self.username}>'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.id}: {self.body}>'
+
+class TodoItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(256))
+    completed = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(32), nullable=False)
+    
+    def __repr__(self):
+        return f'<TodoItem {self.id}: {self.content} {self.completed}>'
+class Email(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   recipient = db.Column(db.String(32), nullable = False)
+   subject = db.Column(db.String(32), nullable = False)
+   body = db.Column(db.String(32), nullable = False)
+   sender = db.Column(db.String(32), nullable =False)
+   def __repr__(self):
+        return f'<Email {self.id}: {self.subject} {self.body}>'
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
